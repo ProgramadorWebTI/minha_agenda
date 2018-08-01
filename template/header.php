eda89a509ca6fef528ceef222c558691de8e18a0
ProgramadorWebTI <programadorwebti@gmail.com>
2018-08-01 19:19:43 -0300

    Cabeçalho da aplicação

---
+++ template/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="">
+        <link rel="stylesheet" href="<?=base_url('assets/node_modules/bootstrap/dist/css/bootstrap.min.css')?>">
+</head>
+<body>
+
