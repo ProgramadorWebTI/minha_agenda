a802076f98efdd2f839c7789a85fa8579fe8e555
ProgramadorWebTI <programadorwebti@gmail.com>
2018-08-01 20:38:15 -0300

    criando a tela de login

--- index.php
+++ index.php
@@ -1,8 +1,28 @@
+<?php require_once './template/header.php';?>
+<div class="container" style="margin: 6% auto;">
+	<div class="row">
+		<div class="col-md-6 col-md-offset-3">
+			<form method="post" action="">
+			<div class="form-group">
+				<input type="text" name="txtEmail" class="form-control" autocomplete="off">
+			</div>
+			<div class="form-group">
+				<input type="password" name="txtSena" class="form-control" autocomplete="off">
+			</div>
+			<div class="form-group">
+				<button type="submit" class="btn btn-primary">logar</button>
+			</div>
+		</form>
+		</div>
+	</div>
+</div>
+
 <?php
 
-require_once './vendor/autoload.php';
+if (isset($_POST['txtEmail'])) {
+	print_r($_POST);
+}
 
-$result = seleciona_tudo();
+?>
 
-print_r($result);
-
+<?php require_once './template/footer.php';?>
