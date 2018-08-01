b24844a36029355c587209d142007597ac8c27a9
ProgramadorWebTI <programadorwebti@gmail.com>
2018-08-01 19:19:59 -0300

    Rodapé do site

---
+++ template/footer.php
@@ -0,0 +1,5 @@
+
+<script type="text/javascript" src="<?= base_url('assets/node_modules/jquery/dist/jquery.min.js')?>"></script>
+<script type="text/javascript" src="<?= base_url('assets/node_modules/bootstrap/dist/js/bootstrap.min.js')?>"></script>
+</body>
+</html>
