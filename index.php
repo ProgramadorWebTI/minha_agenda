451a8a7fef4ecb10a019f2ef7f9ca1b24e95796a
ProgramadorWebTI <programadorwebti@gmail.com>
2018-08-01 21:51:31 -0300

    Ageitando o login

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-<?php require_once './template/header.php';?>
+<?php require_once './template/header.php';
+
+if (isset($_SESSION['usuario_logado'])) {
+	header("location: ./admin.php");
+}
+
+?>
 <div class="container" style="margin: 6% auto;">
 	<div class="row">
 		<div class="col-md-6 col-md-offset-3">
@@ -7,7 +13,7 @@
 				<input type="text" name="txtEmail" class="form-control" autocomplete="off">
 			</div>
 			<div class="form-group">
-				<input type="password" name="txtSena" class="form-control" autocomplete="off">
+				<input type="password" name="txtSenha" class="form-control" autocomplete="off">
 			</div>
 			<div class="form-group">
 				<button type="submit" class="btn btn-primary">logar</button>
@@ -19,8 +25,15 @@
 
 <?php
 
-if (isset($_POST['txtEmail'])) {
-	print_r($_POST);
+if (isset($_POST['txtEmail']) && !empty($_POST['txtEmail'])) {
+	$email = trim($_POST['txtEmail']);
+	$senha = trim($_POST['txtSenha']);
+	if (login($email, $senha)) {
+		$_SESSION['usuario_logado'] = login($email, $senha);
+		header("location: ./admin.php");
+	} else {
+		echo "erro ao logar";
+	}
 }
 
 ?>
