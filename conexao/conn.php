d19dc10d9c11531bd5db9b1f630ae2eadebf7c2d
ProgramadorWebTI <programadorwebti@gmail.com>
2018-08-02 20:24:06 -0300

    Setando o utf-8 para os caracteres não fica zuado no sgbd

--- conexao/conn.php
+++ conexao/conn.php
@@ -1,6 +1,6 @@
 <?php
 
 function pdo() {
-    $db = new \PDO('mysql:host=127.0.0.1;dbname=minha_agenda;', 'root', '');
+    $db = new \PDO('mysql:host=127.0.0.1;dbname=minha_agenda;charset=utf8', 'root', '', array(\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
     return $db;
 }
