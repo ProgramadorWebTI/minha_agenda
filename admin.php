36d79479e14025cc5e3b8d289a3a7567d63fd4b4
ProgramadorWebTI <programadorwebti@gmail.com>
2018-08-02 14:39:39 -0300

    atualizado

--- admin.php
+++ admin.php
@@ -1,29 +1,29 @@
-<?php require_once './template/header.php';?>
+<?php require_once './template/header.php'; ?>
 <?php
-	esta_logado();
+esta_logado();
 ?>
 
 <div class="container">
-	<div class="row">
-		<table class="table table-bordered">
-			<thead>
-				<tr>
-					<th>Nome:</th>
-					<th>Numero:</th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php foreach (seleciona_agenda(ID_USER) as $v): ?>
-									<tr>
-										<td><?=$v->nome_agenda?></td>
-										<td><?=$v->numero_agenda?></td>
-									</tr>
-					<?php endforeach;?>
+    <div class="row">
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>Nome:</th>
+                    <th>Numero:</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach (seleciona_agenda(ID_USER) as $v): ?>
+                    <tr>
+                        <td><?= $v->nome_agenda ?></td>
+                        <td><?= $v->numero_agenda ?></td>
+                    </tr>
+                <?php endforeach; ?>
 
-			</tbody>
-		</table>
-	</div>
+            </tbody>
+        </table>
+    </div>
 </div>
 
 
-<?php require_once './template/footer.php';?>
+<?php require_once './template/footer.php'; ?>
