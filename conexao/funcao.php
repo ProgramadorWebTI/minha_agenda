c5914ac6786e8e239d42353f869a06758148f1ab
ProgramadorWebTI <programadorwebti@gmail.com>
2018-08-02 19:20:35 -0300

    Criando função para adicionar novo contato

--- conexao/funcao.php
+++ conexao/funcao.php
@@ -1,5 +1,15 @@
 <?php
 
+function inserir_novo_contato($id,$nome_agenda,$tipo_agenda,$numero_agenda) {
+    $sql = "INSERT INTO agendas (usuario_id,nome_agenda,tipo_agenda,numero_agenda) VALUES(?,?,?,?)";
+    $stmt  = pdo()->prepare($sql);
+    $stmt->bindValue(1,$id);
+    $stmt->bindValue(2,$nome_agenda);
+    $stmt->bindValue(3,$tipo_agenda);
+    $stmt->bindValue(4,$numero_agenda);
+    return $stmt->execute();
+}
+
 function seleciona_tudo() {
 	$sql = "SELECT * FROM agendas";
 	$stmt = pdo()->prepare($sql);
