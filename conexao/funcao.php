54aaf37c931ab28ae5ad5b88eae8a0fd740adf1b
ProgramadorWebTI <programadorwebti@gmail.com>
2018-08-01 21:51:04 -0300

    Criando função para lista agenda

--- conexao/funcao.php
+++ conexao/funcao.php
@@ -1,10 +1,28 @@
 <?php
 
 function seleciona_tudo() {
-    $sql = "SELECT * FROM agendas";
-    $stmt = pdo()->prepare($sql);
-    $stmt->execute();
-    $resultado = $stmt->fetchAll(\PDO::FETCH_OBJ);
-    return $resultado;   
+	$sql = "SELECT * FROM agendas";
+	$stmt = pdo()->prepare($sql);
+	$stmt->execute();
+	$resultado = $stmt->fetchAll(\PDO::FETCH_OBJ);
+	return $resultado;
 }
 
+function seleciona_agenda($idUser) {
+	$sql = "SELECT * FROM agendas join usuarios on usuarios.id_usuario = agendas.usuario_id  WHERE id_usuario =? ";
+	$stmt = pdo()->prepare($sql);
+	$stmt->bindValue(1, $idUser);
+	$stmt->execute();
+	$resultado = $stmt->fetchAll(\PDO::FETCH_OBJ);
+	return $resultado;
+}
+
+function login($email, $senha) {
+	$sql = "SELECT * FROM usuarios WHERE email_usuario=? AND senha_usuario=?";
+	$stmt = pdo()->prepare($sql);
+	$stmt->bindValue(1, $email);
+	$stmt->bindValue(2, $senha);
+	$stmt->execute();
+	$resultado = $stmt->fetch(\PDO::FETCH_OBJ);
+	return $resultado;
+}
