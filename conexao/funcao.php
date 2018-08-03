d2df3eb5e037b94370bf25052daf589f16e003d1
ProgramadorWebTI <programadorwebti@gmail.com>
2018-08-03 15:56:29 -0300

    atualizando a funcão inserir_novo_contato

--- conexao/funcao.php
+++ conexao/funcao.php
@@ -1,43 +1,43 @@
 <?php
 
 function inserir_novo_contato($id, $nome_agenda, $tipo_agenda, $numero_agenda) {
-    $sql = "INSERT INTO agendas (usuario_id,nome_agenda,tipo_agenda,numero_agenda) VALUES(?,?,?,?)";
-    $stmt = pdo()->prepare($sql);
-    $stmt->bindValue(1, $id);
-    $stmt->bindValue(2, $nome_agenda);
-    $stmt->bindValue(3, $tipo_agenda);
-    $stmt->bindValue(4, $numero_agenda);
-    $stmt->execute();
-    if ($stmt->rowCount()) {
-        $_SESSION['menssagem'] = ' <p class="alert alert-success text-center"> Cadastrado com sucesso! </p>';
-    } else {
-        $_SESSION['menssagem'] = '<p class="alert alert-danger text-center">Erro, não foi possivel cadastrar!</p>';
-    }
+	$sql = "INSERT INTO agendas (usuario_id,nome_agenda,tipo_agenda,numero_agenda) VALUES(?,?,?,?)";
+	$stmt = pdo()->prepare($sql);
+	$stmt->bindValue(1, $id);
+	$stmt->bindValue(2, $nome_agenda);
+	$stmt->bindValue(3, $tipo_agenda);
+	$stmt->bindValue(4, $numero_agenda);
+	$stmt->execute();
+	if ($stmt->rowCount()) {
+		$_SESSION['menssagem'] = ' <p class="alert alert-success text-center"> Cadastrado com sucesso! </p>';
+	} else {
+		$_SESSION['menssagem'] = '<p class="alert alert-danger text-center">Erro, não foi possivel cadastrar!</p>';
+	}
 }
 
 function seleciona_tudo() {
-    $sql = "SELECT * FROM agendas";
-    $stmt = pdo()->prepare($sql);
-    $stmt->execute();
-    $resultado = $stmt->fetchAll(\PDO::FETCH_OBJ);
-    return $resultado;
+	$sql = "SELECT * FROM agendas";
+	$stmt = pdo()->prepare($sql);
+	$stmt->execute();
+	$resultado = $stmt->fetchAll(\PDO::FETCH_OBJ);
+	return $resultado;
 }
 
 function seleciona_agenda($idUser) {
-    $sql = "SELECT * FROM agendas join usuarios on usuarios.id_usuario = agendas.usuario_id  WHERE id_usuario =? ";
-    $stmt = pdo()->prepare($sql);
-    $stmt->bindValue(1, $idUser);
-    $stmt->execute();
-    $resultado = $stmt->fetchAll(\PDO::FETCH_OBJ);
-    return $resultado;
+	$sql = "SELECT * FROM agendas join usuarios on usuarios.id_usuario = agendas.usuario_id  WHERE id_usuario =? ";
+	$stmt = pdo()->prepare($sql);
+	$stmt->bindValue(1, $idUser);
+	$stmt->execute();
+	$resultado = $stmt->fetchAll(\PDO::FETCH_OBJ);
+	return $resultado;
 }
 
 function login($email, $senha) {
-    $sql = "SELECT * FROM usuarios WHERE email_usuario=? AND senha_usuario=?";
-    $stmt = pdo()->prepare($sql);
-    $stmt->bindValue(1, $email);
-    $stmt->bindValue(2, $senha);
-    $stmt->execute();
-    $resultado = $stmt->fetch(\PDO::FETCH_OBJ);
-    return $resultado;
+	$sql = "SELECT * FROM usuarios WHERE email_usuario=? AND senha_usuario=?";
+	$stmt = pdo()->prepare($sql);
+	$stmt->bindValue(1, $email);
+	$stmt->bindValue(2, $senha);
+	$stmt->execute();
+	$resultado = $stmt->fetch(\PDO::FETCH_OBJ);
+	return $resultado;
 }
