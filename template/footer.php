68f1f1fc1a98763757c6b6a20ecac0dc7fc6ce02
ProgramadorWebTI <programadorwebti@gmail.com>
2018-08-02 21:27:00 -0300

    adicionando script jquery-mask

--- template/footer.php
+++ template/footer.php
@@ -1,5 +1,7 @@
 
 <script type="text/javascript" src="<?= base_url('assets/node_modules/jquery/dist/jquery.min.js')?>"></script>
+<script type="text/javascript" src="<?= base_url('assets/node_modules/jquery-mask-plugin/dist/jquery.mask.js')?>"></script>
 <script type="text/javascript" src="<?= base_url('assets/node_modules/bootstrap/dist/js/bootstrap.min.js')?>"></script>
+<script type="text/javascript" src="<?= base_url('assets/script.js')?>"></script>
 </body>
 </html>
