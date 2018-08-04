2ee24ed48d63b2226da2d51c59b7ba7faf82884f
ProgramadorWebTI <programadorwebti@gmail.com>
2018-08-03 22:09:08 -0300

    criado os links de editar e deletar

--- admin.php
+++ admin.php
@@ -1,26 +1,31 @@
-<?php require_once './template/header.php'; ?>
+<?php require_once './template/header.php';?>
 <?php
 esta_logado();
 ?>
 
 <div class="container">
     <div class="row">
-        <a class="btn btn-primary btn=-sm" href="<?= base_url('formulario_contato.php');?>">Novo contato</a>
+        <a class="btn btn-primary btn=-sm" href="<?=base_url('formulario_contato.php');?>">Novo contato</a>
         <hr>
         <table class="table table-bordered">
             <thead>
                 <tr>
                     <th>Nome:</th>
                     <th>Numero:</th>
+                    <th></th>
                 </tr>
             </thead>
             <tbody>
                 <?php foreach (seleciona_agenda(ID_USER) as $v): ?>
                     <tr>
-                        <td><?= $v->nome_agenda ?></td>
-                        <td class="<?php if($v->tipo_agenda == 0){echo "phone_with_ddd";}else{echo "phone";} ?>"><?= $v->numero_agenda ?></td>
+                        <td><?=$v->nome_agenda?></td>
+                        <td class="<?php if ($v->tipo_agenda == 0) {echo "phone_with_ddd";} else {echo "phone";}?>"><?=$v->numero_agenda?></td>
+                        <td>
+                            <a href="">Editar</a>
+                            <a href="/deletar.php?id=<?=$v->id_agenda?>">Deletar</a>
+                        </td>
                     </tr>
-                <?php endforeach; ?>
+                <?php endforeach;?>
 
             </tbody>
         </table>
@@ -28,4 +33,4 @@ esta_logado();
 </div>
 
 
-<?php require_once './template/footer.php'; ?>
+<?php require_once './template/footer.php';?>
